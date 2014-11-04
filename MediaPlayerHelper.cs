df5a5a0520ed5356963b4e58ab35fdf527a6dfb8
Cprice <thrashers231@yahoo.com>
2014-11-03 19:37:15 -0500

    initial commit

---
+++ MediaPlayerHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayerHelper
+{
+    public class MediaPlayerHelper
+    {
+
+        private static MediaPlayerHelper instance;
+
+        public static MediaPlayerHelper Instance
+        {
+            get
+            {
+                if(instance == null)
+                    instance = new MediaPlayerHelper();
+
+                return instance;
+            }
+        }
+
+        public bool Loop
+        {
+            get
+            {
+                return _loop;
+            }
+            set 
+            {
+                _wplayer.settings.setMode("Loop", value);
+                _loop = value;
+            } 
+        }
+
+        bool _loop;
+        System.Media.SoundPlayer _player;
+        WMPLib.WindowsMediaPlayer _wplayer;
+        bool _isPlaying;
+
+        private MediaPlayerHelper()
+        {
+            _player = new System.Media.SoundPlayer();
+            _wplayer = new WMPLib.WindowsMediaPlayer();
+            Loop = false;
+            _isPlaying = false;
+        }
+
+        /// <summary>
+        /// Plays a song, only 1 song can be played at a time and can be stopped using the Stop() method
+        /// </summary>
+        /// <param name="s">Song to play</param>
+        public void Play(SongFile s)
+        {
+            if (_isPlaying)
+            {
+                _player.Stop();
+                _wplayer.controls.stop();
+            }
+
+            if (Path.GetExtension(s.File) == ".wav")
+            {
+                _player.SoundLocation = s.File;
+
+                if (_loop)
+                    _player.PlayLooping();
+                else
+                    _player.Play();
+
+                _isPlaying = true;
+            }
+            else if (Path.GetExtension(s.File) == ".mp3")
+            {
+                _wplayer.URL = s.File;
+                _wplayer.controls.play();
+                _isPlaying = true;
+            }
+            else if (Path.GetExtension(s.File) == ".wma")
+            {
+                _wplayer.URL = s.File;
+                _wplayer.controls.play();
+                _isPlaying = true;
+            }
+        }
+
+        /// <summary>
+        /// Stops the current song that is being played
+        /// </summary>
+        public void Stop()
+        {
+            if (_isPlaying)
+            {
+                _player.Stop();
+                _wplayer.controls.stop();
+            }
+        }
+
+        /// <summary>
+        /// Plays a new sound independent from the normal sound logic. Playing a new sound will not cancel a song or sound.
+        /// </summary>
+        public void PlaySound(SongFile s)
+        {
+            if (Path.GetExtension(s.File) == "wav")
+            {
+                _player.SoundLocation = s.File;
+
+                if (_loop)
+                    _player.PlayLooping();
+                else
+                    _player.Play();
+
+                _isPlaying = true;
+            }
+            else if (Path.GetExtension(s.File) == "mp3")
+            {
+                _wplayer.URL = s.File;
+                _wplayer.controls.play();
+                _isPlaying = true;
+            }
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayerHelper
+{
+    public class SongFile
+    {
+        public string File { get; set; }
+
+        public SongFile(string s)
+        {
+            File = s;
+        }
+    }
+}
